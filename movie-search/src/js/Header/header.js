2ce43bbc67a53508625ffa2c9653b83bb51b55e9
Dzianis Subach <suboch.denny@gmail.com>
2020-05-04 23:45:08 +0300

    feat: add class for logo

--- movie-search/src/js/Header/header.js
+++ movie-search/src/js/Header/header.js
@@ -7,6 +7,7 @@ export default function createHeader() {
     const headerWrap = document.createElement('div');
     headerWrap.classList.add('header__wrap');
     const logo = document.createElement('h1');
+    logo.classList.add('header__logo');
     logo.textContent = "Movie Search";
     headerWrap.append(logo);
     wrapper.append(headerWrap);
