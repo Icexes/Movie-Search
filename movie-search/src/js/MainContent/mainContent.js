8256576fc78165b7e215e7112bf01e4420036751
Dzianis Subach <suboch.denny@gmail.com>
2020-05-10 22:07:12 +0300

    feat: implement movie search

--- movie-search/src/js/MainContent/mainContent.js
+++ movie-search/src/js/MainContent/mainContent.js
@@ -3,10 +3,6 @@ import createKeyBoard from './Keyboard/Keyboard';
 
 export default function createMainContent() {
 
-//     <form action="" method="get">
-//     <input name="s" placeholder="Искать здесь..." type="search">
-//     <button type="submit">Поиск</button>
-//   </form>
     const main = document.createElement('main');
     main.classList.add('main');
     const wrapper = document.createElement('div');
@@ -27,26 +23,33 @@ export default function createMainContent() {
     const searchButton = document.createElement('button');
     searchButton.classList.add('search-form__submit');
     searchButton.textContent = 'Search';
-    const keyboardButton = document.createElement('button');
-    keyboardButton.classList.add('search-form__keyboard-btn');
-    keyboardButton.addEventListener('click', (evt) => {
+    const keyboardBtn = document.createElement('button');
+    keyboardBtn.classList.add('search-form__keyboard-btn');
+    keyboardBtn.addEventListener('click', (evt) => {
         evt.preventDefault();
         const keyboard = document.querySelector('.keyboard');
         keyboard.classList.toggle('keyboard--show');
     })
+    const constrolsButtons = document.createElement('div');
+    constrolsButtons.classList.add('search-form__controls');
     const clearInputBtn = document.createElement('button');
     clearInputBtn.classList.add('search-form__clear-input');
+
+    const voiceRecorderBtn = document.createElement('button');
+    voiceRecorderBtn.classList.add('search-form__voice-recorder');
+    constrolsButtons.append(searchButton,voiceRecorderBtn);
+    searchFormWrapper.append(inputRow, constrolsButtons, keyboardBtn, clearInputBtn);
+    searchForm.append(searchFormWrapper);
+
+    
+    const slider = createSlider();
     const spinner = document.createElement('div');
     for (let i = 0; i< 3; i +=1) {
         const div = document.createElement('div');
         spinner.append(div);
     }
     spinner.classList.add('spinner');
-    searchFormWrapper.append(inputRow, searchButton, keyboardButton, clearInputBtn, spinner);
-    searchForm.append(searchFormWrapper);
-
-    
-    const slider = createSlider();
+    slider.append(spinner);
 
     const logWindow = document.createElement('div');
     logWindow.classList.add('log-window');
--- movie-search/src/js/index.js
+++ movie-search/src/js/index.js
@@ -1,100 +1,109 @@
-import Swiper from '../../node_modules/swiper/js/swiper'
+import Swiper from '../../node_modules/swiper/js/swiper';
 import '../css/style.css';
 import '../css/style.scss';
 import createHeader from './Header/header';
-import createMainContent from './MainContent/mainContent'
+import createMainContent from './MainContent/mainContent';
+import createFooter from './Footer/Footer';
+import createSpeechRecognition from './MainContent/SpeechRecognition/SpeechRecognition';
+import playAudio from './MainContent/PlayAudio/PlayAudio';
+import isRussian from './Helper/IsRussian'
 
- 
-    // const key = 'bfcf5d6';
-    const key2 = 'e4642a3b'
-    let currentPage = 1;
-    let totalPages = 0;
-    let previousStateCurrentPage = currentPage;
-    let request = 'home';
-    let prevRequest = request;
-    const getMovieTitle = (page, keyValue, requestData) => {
+class ApiError extends Error {
+    constructor(message, fromPromise) {
+        super(message);
+        this.fromApi = fromPromise;
+    }
+}
+const key = 'e4642a3b'
+let currentPage = 1;
+let totalPages = 0;
+let previousStateCurrentPage = currentPage;
+let request = 'home';
+let prevRequest = request;
+
+const getMovieTitle = (page, keyValue, requestData) => {
     const url = `https://www.omdbapi.com/?s=${requestData}&type=movie&page=${page}&apikey=${keyValue}`;
 
     return fetch(url)
-    .then(res => res.json());
+        .then(res => res.json())
+        .then(data => {
+            if (data.Response === 'True') {
+                const searchResult = data.Search;
+                totalPages = Math.round(data.totalResults / 10);
+                return searchResult;
+            }
+            throw new ApiError(data.Error, 'omdbapi');
+        })
 }
-
 const getRate = (imdbID, keyValue) => {
     const url = `https://www.omdbapi.com/?i=${imdbID}&apikey=${keyValue}`;
-
     return fetch(url)
-    .then(res => res.json())
-    .then(data => data.imdbRating);
-    
+        .then(res => res.json())
+        .then(data => {
+            if (data.Response === 'True') {
+                return data.imdbRating
+            }
+            throw new ApiError(data.Error, 'omdbapi');
+        });
+
 }
 const getTranslateWord = (word) => {
     const url = `https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20200322T155651Z.de98a60e6a99185e.089aea4237b51c6db082c966f27a7895cd1e8b44&text=${word}&lang=ru-en`;
-
     return fetch(url)
-    .then(res => res.json())
-    .then(data => data.text[0]);
+        .then(res => res.json())
+        .then(translateData => {
+            if (translateData.code >= 400) throw new ApiError(translateData.message, 'yandex');
+            return translateData.text[0]
+        })
 }
-
-
 createHeader();
 createMainContent();
+createFooter();
 const textInput = document.querySelector('.search-form__input-row');
+textInput.focus();
 const logWindow = document.querySelector('.log-window');
-const searchFormSubmitBtn = document.querySelector('.search-form__submit');
-const swiper = new Swiper ('.swiper-container', {
-    // Optional parameters
-    // slidesPerView: 4,
+const searchForm = document.querySelector('.search-form');
+const swiperContainer = document.querySelector('.swiper-container');
+const swiper = new Swiper('.swiper-container', {
     centerInsufficientSlides: true,
-   
-  //  spaceBetween: 20,
     breakpoints: {
         320: {
             slidesPerView: 1,
-            spaceBetween: 30,
+            spaceBetween: 100,
         },
-        700: {
+        760: {
             slidesPerView: 2,
             spaceBetween: 30,
         },
         1100: {
             slidesPerView: 3,
-            spaceBetween: 20,
+            spaceBetween: 25,
         },
-        1350 : {
+        1350: {
             slidesPerView: 4,
-            spaceBetween: 20,
+            spaceBetween: 25,
         }
     },
-    
+
     // If we need pagination
     pagination: {
-      el: '.swiper-pagination',
+        el: '.swiper-pagination',
     },
 
     // Navigation arrows
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
     },
 
     // And if we need scrollbar
     scrollbar: {
-      el: '.swiper-scrollbar',
+        el: '.swiper-scrollbar',
     },
-  })
+})
 
-  class NotFoundDataError extends Error{
-      constructor (message, fromPromise) {
-          super(message);
-          this.name = fromPromise; 
-      }
+const createMovieCard = (title, year, imgSrc, rate, id) => {
 
-  }
-
- 
-
-  const createMovieCard = (title, year, imgSrc, rate, id) => {
-    
     const cardImageSrc = imgSrc;
     const slide = document.createElement('div');
     slide.classList.add('swiper-slide');
@@ -103,8 +112,10 @@ const swiper = new Swiper ('.swiper-container', {
     card.href = `https://www.imdb.com/title/${id}/videogallery/`;
     const poster = document.createElement('img');
     poster.classList.add('card-film__poster');
-    poster.src = cardImageSrc === 'N/A' ? 'img/not-found.jpg' : cardImageSrc;
-    poster.onerror = () => {poster.src = 'img/not-found.jpg'};
+    poster.src = cardImageSrc === 'N/A' ? 'img/no-poster.png' : cardImageSrc;
+    poster.onerror = () => {
+        poster.src = 'img/no-poster.png'
+    };
     const filmName = document.createElement('p');
     filmName.classList.add('card-film__name');
     filmName.textContent = title;
@@ -122,82 +133,101 @@ const swiper = new Swiper ('.swiper-container', {
     return slide;
 }
 
-  const getMovies = () => {
+const getMovies = () => {
     const spinner = document.querySelector('.spinner');
     spinner.classList.add('spinner--show');
     getTranslateWord(request)
-    .then(translate => {
-        console.log(translate);
-      return  getMovieTitle(currentPage, key2, translate)})
-        .then(data => {
-            if (data.Response === 'True') {
-                console.log('yes wse zaebis');
-           const searchResult = data.Search;
-
-           totalPages = Math.round(data.totalResults/10);
-           console.log(totalPages);
-        //    console.log(totalResults);
-           return searchResult;
-        }
-        throw new NotFoundDataError(data.Error,'getMovieTitle');
-        })
+        .then(translate => getMovieTitle(currentPage, key, translate))
         .then(filmInfo => {
-
-            const rates = filmInfo.map(film => getRate(film.imdbID, key2));
+            const rates = filmInfo.map(film => getRate(film.imdbID, key));
             return Promise.all(rates).then(ratesArr => {
-                return  {ratesArr,filmInfo};
+                return {
+                    ratesArr,
+                    filmInfo
+                };
             });
-            })
-            .then (cardsData => {
-                const cards = [];
-                cardsData.filmInfo.forEach((film, index) => cards.push(createMovieCard(film.Title, film.Year, film.Poster, cardsData.ratesArr[index], film.imdbID)))
-                if (currentPage === 1 && cardsData.filmInfo.length !== 0) {swiper.removeAllSlides(); swiper.update()}
-                   swiper.appendSlide(cards);
-                   swiper.update();
-                
-                  
-            }).catch((err) => { 
-           currentPage = previousStateCurrentPage;
-           if (err instanceof NotFoundDataError) logWindow.textContent = `${err.message.slice(0,-1)} for '${request}'`;
-           else logWindow.textContent = err.message;
-           
-           
-           request = prevRequest;
-           
+        })
+        .then(cardsData => {
+            const cards = [];
+            cardsData.filmInfo.forEach((film, index) => cards.push(createMovieCard(film.Title, film.Year, film.Poster, cardsData.ratesArr[index], film.imdbID)))
+            if (currentPage === 1 && cardsData.filmInfo.length !== 0) {
+                swiper.removeAllSlides();
+                swiper.update()
+            }
+            swiper.appendSlide(cards);
+            swiper.update();
+        }).catch((err) => {
+            setTimeout(() => {
+                currentPage = previousStateCurrentPage;
+                if (err instanceof ApiError) {
+                    if (err.fromApi === 'yandex') {
+                        logWindow.textContent = `Yandex API Error: ${err.message}`;
+                    } else if (err.fromApi === 'omdbapi') {
+                        if (err.message.includes('not found')) {
+                            logWindow.textContent = `No results were found for '${request}'`;
+                        } else logWindow.textContent = `${err.message.slice(0,-1)} for '${request}'`;
+                    }
+                } else logWindow.textContent = err.message;
+                request = prevRequest;
+            }, 500);
         })
         .finally(() => {
-            spinner.classList.remove('spinner--show');
-            textInput.value = '';
+            setTimeout(() => {
+                spinner.classList.remove('spinner--show');
+                swiperContainer.classList.remove('swiper-container--hidden');
+            }, 500);
+
         })
 
 }
 
 getMovies();
 swiper.on('reachEnd', () => {
-     if (document.querySelector(".swiper-slide") === null) return;
+    if (document.querySelector(".swiper-slide") === null) return;
     currentPage += 1;
-     if (currentPage <= totalPages) getMovies();
-      
-    
+    if (currentPage <= totalPages) getMovies();
 });
 
 
-searchFormSubmitBtn.addEventListener('click', (evt) => {
-    evt.preventDefault();
-    if (textInput.value === '') return;
-    previousStateCurrentPage = currentPage;
-    currentPage = 1;
-    prevRequest = request;
-    request = textInput.value;
-    logWindow.textContent = /[а-яА-Я]+/g.test(request) ? `Results for '${request}'` : '';
-    getMovies();
-});
-
-const clearInputBtn = document.querySelector('.search-form__clear-input');
-clearInputBtn.addEventListener('click', (evt) => {
-    evt.preventDefault();
-    textInput.value = '';
-
+const recognition = createSpeechRecognition();
+recognition.addEventListener('result', (event) => {
+    playAudio('audio/end.mp3');
+    recognition.stop();
+    let result = '';
+    for (let i = event.resultIndex; i < event.results.length; i += 1) {
+        result += event.results[i][0].transcript;
+    }
+    textInput.value = result;
+    const click = new Event('click', {
+        "bubbles": true,
+        "cancelable": false
+    });
+    document.querySelector('.search-form__submit').dispatchEvent(click);
 })
 
-// setTimeout(() => window.location.reload(true), 500) 
+recognition.addEventListener('error', (event) => {
+    playAudio('audio/err.mp3');
+    logWindow.textContent = `Error occurred in recognition: ${event.error}`;
+});
+
+searchForm.addEventListener('click', (event) => {
+    event.preventDefault();
+    if (event.target.closest('.search-form__clear-input')) {
+        textInput.value = '';
+    } else if (event.target.closest('.search-form__submit')) {
+        if (textInput.value === '') return;
+        if (document.querySelector('.keyboard--show')) {
+            document.querySelector('.keyboard--show').classList.remove('keyboard--show');
+        }
+        previousStateCurrentPage = currentPage;
+        currentPage = 1;
+        prevRequest = request;
+        request = textInput.value;
+        logWindow.textContent = isRussian(request) ? `Showing results for '${request}'` : '';
+        swiperContainer.classList.add('swiper-container--hidden');
+        getMovies();
+    } else if (event.target.closest('.search-form__voice-recorder')) {
+        playAudio('audio/start.mp3');
+        recognition.start();
+    }
+});
