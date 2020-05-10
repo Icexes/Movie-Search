0965125f2450778e3aeb1a59be2041a36fe352e1
Dzianis Subach <suboch.denny@gmail.com>
2020-05-10 22:05:40 +0300

    refactor: delete comments

--- movie-search/src/js/MainContent/Slider/Slider.js
+++ movie-search/src/js/MainContent/Slider/Slider.js
@@ -1,35 +1,6 @@
 export default function createSlider() {
-
-    // const url = `https://www.omdbapi.com/?s=dream&page=${page}&apikey=bfcf5d6`;
-
-    // return fetch(url)
-    //   .then(res => res.json())
-    //   .then(data => {
-    //     console.log(data)
-    //   });
-     
-//      <div class="swiper-container">
-//      <!-- Additional required wrapper -->
-//      <div class="swiper-wrapper">
-//          <!-- Slides -->
-//          <div class="swiper-slide">Slide 1</div>
-//          <div class="swiper-slide">Slide 2</div>
-//          <div class="swiper-slide">Slide 3</div>
-//          ...
-//      </div>
-//      <!-- If we need pagination -->
-//      <!-- <div class="swiper-pagination"></div> -->
- 
-//      <!-- If we need navigation buttons -->
-//      <div class="swiper-button-prev"></div>
-//      <div class="swiper-button-next"></div>
- 
-//      <!-- If we need scrollbar -->
-//      <!-- <div class="swiper-scrollbar"></div> -->
-//  </div> 
     const swiper = document.createElement('div');
-    swiper.classList.add('swiper');
-    
+    swiper.classList.add('swiper');  
     const swiperContainer = document.createElement('div');
     swiperContainer.classList.add('swiper-container');
     const swiperWrapper = document.createElement('div');
@@ -39,12 +10,8 @@ export default function createSlider() {
     const swiperBtnNext = document.createElement('div');
     swiperBtnNext.classList.add('swiper-button-next');
     const scrollbar = document.createElement('div');
-    scrollbar.classList.add('swiper-scrollbar');
-
-    
-    
+    scrollbar.classList.add('swiper-scrollbar'); 
     swiperContainer.append(swiperWrapper ,scrollbar);
     swiper.append(swiperContainer,swiperBtnPrev, swiperBtnNext);
     return swiper;
-
 }
